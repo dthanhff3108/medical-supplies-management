5c8c2454b8710cca2c50a84782e7f4b30e2c06a6
dthanhff3108 <duythanhtizzer@gmail.com>
2022-10-23 11:08:03 +0700

    project base v1

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -3,13 +3,13 @@ import { initReactI18next } from 'react-i18next';
 
 import en from './en/translation.json';
 import vn from './vn/translation.json';
-import { convertLanguageJsonToObject } from './translations';
+import { convertJsonToObject } from './translations';
 
 export const LANGUAGE_EN = 'en';
 export const LANGUAGE_VN = 'vn';
 // Create the 'translations' object to provide full intellisense support for the static json files.
-convertLanguageJsonToObject(en);
-convertLanguageJsonToObject(vn);
+convertJsonToObject(en);
+convertJsonToObject(vn);
 
 export const translationsJson = {
   [LANGUAGE_EN]: {
@@ -26,10 +26,6 @@ export const DEFAULT_LANGUAGE = LANGUAGE_EN;
 i18next
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources: translationsJson,
     fallbackLng: DEFAULT_LANGUAGE,
--- src/locales/translations.ts
+++ src/locales/translations.ts
@@ -1,18 +1,10 @@
 import { ConvertedToObjectType, TranslationJsonType } from './types';
 
-/**
- * This file is seperate from the './i18n.ts' simply to make the Hot Module Replacement work seamlessly.
- * Your components can import this file in 'messages.ts' files which would ruin the HMR if this isn't a separate module
- */
+
 export const translations: ConvertedToObjectType<TranslationJsonType> = {} as any;
 
-/*
- * Converts the static JSON file into an object where keys are identical
- * but values are strings concatenated according to syntax.
- * This is helpful when using the JSON file keys and still having the intellisense support
- * along with type-safety
- */
-export const convertLanguageJsonToObject = (
+
+export const convertJsonToObject = (
   json: any,
   objToConvertTo = translations,
   current?: string,
@@ -21,7 +13,7 @@ export const convertLanguageJsonToObject = (
     const currentLookupKey = current ? `${current}.${key}` : key;
     if (typeof json[key] === 'object') {
       objToConvertTo[key] = {};
-      convertLanguageJsonToObject(json[key], objToConvertTo[key], currentLookupKey);
+      convertJsonToObject(json[key], objToConvertTo[key], currentLookupKey);
     } else {
       objToConvertTo[key] = currentLookupKey;
     }
